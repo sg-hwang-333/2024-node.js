a9df20a599fd51d819becff91c6ef2c93cf8af5f
sg-hwang-333 <hsg151515@naver.com>
2024-03-14 15:02:42 +0900

    [0314] 데이터 타입 false 나오는 것들 (null, NaN, 0, 빈문자열, undefined)

---
+++ ex03.js
@@ -0,0 +1,9 @@
+console.log("Data Types"); 
+// 전부 false
+console.log("null : " + Boolean(null));  
+console.log("NaN : " + Boolean(NaN));
+console.log("0 : " + Boolean(0));
+console.log("빈문자열(\"\") : " + Boolean(""));
+console.log("빈문자열(\'\') : " + Boolean(''));
+console.log("빈문자열(\`\`) : " + Boolean(``));
+console.log("undefined : " + Boolean(undefined));
