d3a495abbd74b7cbca9367bb5c37c851d37a4828
sg-hwang-333 <hsg151515@naver.com>
2024-04-18 14:51:29 +0900

    [0418] app.use(), app.all()

--- express/index.js
+++ express/index.js
@@ -22,14 +22,44 @@ const middle1 = require('./my_modules/middle1')
 app.use(middle1);
 
 
+app.all("/all", (req, res, next) => {
+    res.send("all");
+    next();
+}) 
+
+app.use("/use", (req, res, next) => {
+    res.send("use");
+    next();
+}) 
+
+
 // 기본 경로 시 호출
-app.get('/', function(req, res) {
+app.get('/', function(req, res, next) {
     console.log("경로 : ", path.join(__dirname, "html"));
-    res.send("Hello World!")
+    //res.send("Hello World!")
+    
+    next();
 })
 
-app.get('/home', function (req, res) {
+app.get('/', function(req, res) {
+    console.log("두번째 / 입니다");
+    res.send("Hello World2!")
+})
+
+app.get('/home', function (req, res, next) {
+    // 애플리케이션 수준의 상태 저장
+    req.app.locals.message = 'Hello, World~~';
+    // res.sendFile(__dirname+"/home.html")
+    next();
+}, (req, res) => {
+    // 응답 수준의 상태 저장
+    res.locals.additionalMessage = 'swag';
+    // 저장된 상태 값 사용
+    const message = `${req.app.locals.message} ${res.locals.additionalMessage}`;
+    console.log(message);
+
     res.sendFile(__dirname+"/home.html")
+
 });
 
 // : 뒤에 있는 것은 어떤 텍스트를 넣든 그 이전 경로로 호출됨
