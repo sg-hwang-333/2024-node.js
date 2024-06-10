05ca1c3d9ae0020b51afc29af5c3f37b0417c17c
sg-hwang-333 <hsg151515@naver.com>
2024-06-10 11:15:25 +0900

    [0603] ejs, cors, morgan, dotenv, express 실습

--- express/board/board_router.js
+++ express/board/board_router.js
@@ -1,45 +1,19 @@
 //이거 board 폴더 안에 있어욥~!~~!~!
 //npm install express
 const express = require("express");
-const app = express();
- //npm install dotenv
-const dotenv = require("dotenv").config(); //기본값 .env파일 생성함. 그 안에 설정값 기입
-
- //npm install morgan
-const morgan  = require("morgan");
-app.use(morgan('dev'));
-
- //npm install cors
-const cors    = require("cors");
-app.use(cors({
-    origin: "http://127.0.0.1:5500",
-    credentials:true
-}));
-
-// 포트 설정
-app.set('port', process.env.PORT || 8080 );
-
-app.listen(app.get('port'),()=>{
-    console.log(app.get('port'),"번 포트에서 서버 실행");
-})
-
-app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+const router = express.Router();
 
 // 게시글 데이터
 let boardList = [];
 let numOfBoard = 1;
 
 // 게시글 API
-app.get('/',(req,res)=>{
-    res.send("게시글 API 확인");
+router.get("/",(req,res)=>{
+    res.render('boardList',{"data":boardList});
+//    res.send(boardList);
 });
 
-app.get("/board",(req,res)=>{
-    res.send(boardList);
-});
-
-app.post("/board",(req,res)=>{
+router.post("/",(req,res)=>{
     const board = {
         "id": numOfBoard++,
         "user_id":req.body.user_id,
@@ -51,7 +25,7 @@ app.post("/board",(req,res)=>{
     res.send("글이 등록되었습니다.");
 });
 
-app.delete("/board/:id",(req,res)=>{
+router.delete("/:id",(req,res)=>{
     //req.params.id 값 리스트 삭제
     //찾는 요소 findItem에 넣기
     const findItem = boardList.find((item)=>{
@@ -62,7 +36,7 @@ app.delete("/board/:id",(req,res)=>{
 
     res.send("글이 삭제되었습니다.");
 });
-app.put("/board/:id",(req,res)=>{
+router.put("/:id",(req,res)=>{
     const findItem = boardList.find((item)=>{
         return item.id == req.params.id
     });
@@ -79,5 +53,7 @@ app.put("/board/:id",(req,res)=>{
     boardList.push(board);
     res.send("글이 수정되었습니다.");
 });
+
+module.exports = router;
 
 
