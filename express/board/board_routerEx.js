180c0fa8310ffe0bc56f62d43cebff94315f9b27
sg-hwang-333 <hsg151515@naver.com>
2024-06-13 19:25:06 +0900

    [0613] 코드 리팩토링

--- express/board/board_routerEx.js
+++ express/board/board_routerEx.js
@@ -10,7 +10,7 @@ const morgan  = require("morgan");
 app.use(morgan('dev'));
 
  //npm install cors
-const cors    = require("cors");
+const cors = require("cors");
 app.use(cors({
     origin: "http://127.0.0.1:5500",
     credentials:true
@@ -33,4 +33,3 @@ app.use(express.urlencoded({extended:true}));
 
 const boardRouter = require('./board_router');
 app.use('/board',boardRouter);
-
--- express/hash/bcrypt.js
+++ express/hash/bcrypt.js
@@ -1,24 +1,26 @@
 // npm install bcrypt
 const bcrypt = require('bcrypt');
+// 블로피시(Blowfish) 암호에 기반을 둔 암호화 해시 함수
 const saltRounds = 10;
-const myPllaintextPassword = 'my password';
+const myPlaintextPassword = 'my password';
 
 async function hashPassword() {
-    try {
-        const hash = await bcrypt.hash(myPllaintextPassword, saltRounds);
+  try {
+    const hash = await bcrypt.hash(myPlaintextPassword, saltRounds);
 
-        // 해시 값을 메모리에 저장
-        const hashPassword = hash;
-        console.log("Hashed Password : ", hashPassword);
+    // 해시 값을 메모리에 저장
+    const hashedPassword = hash;
+    console.log('Hashed Password:', hashedPassword);
 
-        // 비밀번호 비교
-        const inputPassword = "my password"
-        const match = await bcrypt.compare(inputPassword);
-        console.log("password match result : ", match);
-    } catch (err) {
-        // 해당 블록에서 에러에 대한 기록을 남김
-        console.error('Error : ', err)
-    }
-
-
+    // 비밀번호 비교
+    const inputPassword = 'my password';
+    const match = await bcrypt.compare(inputPassword, hashedPassword);
+    console.log('Password match result:', match);
+  }
+  // 해당 블록에서 에러에 대한 기록을 남김 
+  catch (err) {
+    console.error('Error:', err);
+  }
 }
+
+hashPassword();
