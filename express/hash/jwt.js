67b99d89c85f0ece9986ae7ea82d1851f17f61f3
sg-hwang-333 <hsg151515@naver.com>
2024-06-24 11:58:43 +0900

    [0624] JWT (jsonwebtoken) 사용

---
+++ express/hash/jwt.js
@@ -0,0 +1,27 @@
+// npm i jsonwebtoken
+const e = require('express');
+const jwt = require('jsonwebtoken');
+
+// JWT 발급 비밀키
+const secret = 'mySecretKey';
+
+// 페이로드 객체 
+const payload = {
+    'id': '1',
+    'username': 'john',
+    'roles': 'admin'
+};
+
+const token = jwt.sign(payload, secret, {expiresIn: '1h', algorithm: "HS256"});
+
+// 토큰 검증하기
+jwt.verify(token, secret, (err, decode) => {
+    if(err) {
+        console.error(err);
+        return;
+    }
+    // 디코딩된 페이로드 출력
+    console.log(decode);
+})
+
+console.log(token);
